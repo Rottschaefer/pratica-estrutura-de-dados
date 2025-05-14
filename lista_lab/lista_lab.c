2cbfbf8f6e9baf98fa40150e98f3ebf1612f7fb9
Rottschaefer <rottschaefer54@gmail.com>
2025-05-14 19:03:36 -0300

    Questao 02

--- lista_lab/lista_lab.c
+++ lista_lab/lista_lab.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "../TAB/TAB.h"
+#include "../libs/TABB/TABB.h"
 
 
 
@@ -7,22 +7,23 @@ int main(){
 
     int vetor[] = {42, 17, 93, 5, 61, 28, 74, 36, 9, 50, 83, 21, 47, 69, 12, 38, 95, 63, 7, 81};
 
-    TAB* tab = cria_ab_from_vector(vetor, 0, (sizeof(vetor)/sizeof(vetor[0]))-1);
-
-    // TAB_imp_ident(tab);
-
+    TABB* tabb = TABB_inicializa();
 
     int tam = sizeof(vetor)/sizeof(vetor[0]);
 
-    int* res = mN(tab, 47, &tam);
+    for (int i = 0; i < tam; i++)
+    {
+        tabb = TABB_insere(tabb, vetor[i]);
+    }
+    
+    int* res = mN(tabb, 10, &tam);
 
     for (int i = 0; i < tam; i++)
     {
         printf("%d\n", res[i]);
     }
     
-    
-
+    // TABB_imp_ident(tabb);
 
     return 0;
 }
