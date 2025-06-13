c19702d6a3ddb3304163428090d09855a87d0d2f
Rottschaefer <rottschaefer54@gmail.com>
2025-06-13 20:18:27 -0300

    Avanço no script de insercao na arvore B

--- libs/TARVB/TARVB.c
+++ libs/TARVB/TARVB.c
@@ -1,6 +1,7 @@
 #include "TARVB.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
 
 TARVB* inicializa(){
     return NULL;
@@ -11,11 +12,16 @@ TARVB* cria_no(int t){
     no->folha = 1;
     no->chave = (int*)malloc(sizeof(int)*(2*t-1));
     no->filho = (TARVB**)malloc(sizeof(TARVB*)*(2*t));
+    no->nchaves = 0;
 
-    for (int i = 0; i < (2*t); i++)
+    for (int i = 0; i < (2*t - 1); i++)
     {
         no->filho[i] = NULL;
+        no->chave[i] = INT_MIN;
     }
+
+    no->filho[(2*t - 1)] = NULL;
+
 
     return no;
 }
@@ -39,14 +45,76 @@ void TARVB_Imprime(TARVB *a){
 
 void insere_chave_array(TARVB* arvb, int n){
 
-    int i = 0;
+    int i = arvb->nchaves -1;
 
-    while(i<= 0 && arvb->chave[i] > n){
+    while(i >= 0 && arvb->chave[i] > n){
         arvb->chave[i+1] = arvb->chave[i];
+        i--;
     }
+
+    arvb->chave[i+1] = n;
+}
+
+TARVB* Divide(TARVB* pai, TARVB* filho, int t){
+
+
+    //Sobe a chave do meio para o pai
+    int pos = pai->nchaves -1;
+
+    if(pai->nchaves == 0){
+        pai->chave[pos] = filho->chave[t-1];
+        pai->nchaves++;
+        pos++;
+    }
+    else{
+        while(pos >= 0 && pai->chave[pos] > filho->chave[t-1]){
+        pai->chave[pos+1] = pai->chave[pos];
+        pai->filho[pos+2] = pai->filho[pos+1];
+        pos--;
+    }
+
+        pos++;
+        pai->chave[pos] = filho->chave[t-1];
+        pai->nchaves++;
+
+    }
+ 
+    
+
+    
+
+    //Cria novo nó e transfere a metade superior das chaves e ponteiros do filho para ele
+    TARVB* novo_filho = cria_no(t);
+    novo_filho->nchaves= t-1;
+    novo_filho->folha = filho->folha;
+
+    int i = t;
+    for (i = t; i < (2*t-1); i++)
+    {
+        novo_filho->chave[i-t] = filho->chave[i];
+        novo_filho->filho[i-t] = filho->filho[i];
+        filho->filho[i] = NULL;
+    }
+
+    novo_filho->filho[i] = filho->filho[2*t];
+    filho->filho[2*t] = NULL;
+
+
+    filho->nchaves = t-1;
+    
+    //Ponteiros ao redor do valor que subiu recebem os filhos
+    pai->filho[pos] = filho;
+    pai->filho[pos+1] = novo_filho;
+
+    return pai;
+
+    
 }
 
 TARVB* TARVB_insere(TARVB* arvb, int num, int t){
+
+
+    //Caso 1: Árvore Vazia
     if(!arvb){
         arvb = cria_no(t);
         arvb->chave[0] = num;
@@ -54,11 +122,36 @@ TARVB* TARVB_insere(TARVB* arvb, int num, int t){
         return arvb;
     }
 
-    if(arvb->nchaves == (2*t -1)){
-        TARVB * subido
+    //Caso 2: Raíz cheia
+    if(arvb->nchaves == (2*t - 1)){
+
+        printf("Raiz cheia");
+        TARVB* novo_pai = cria_no(t);
+        novo_pai->folha = 0;
+        novo_pai->filho[0] = arvb;  // Primeiro filho é a antiga raiz
+
+        
+        arvb = Divide(novo_pai, arvb, t);
+        
+        if(num > arvb->chave[0]) arvb->filho[1] = TARVB_insere(arvb->filho[1], num, t);
+        else arvb->filho[0] = TARVB_insere(arvb->filho[0], num, t);
+
+        return arvb;
+        
     }
 
-    if(arvb->nchaves < (2*t-1)){
+    //Checa os filhos do nó para saber se algum está cheio
+    if(!arvb->folha){
+        for (int i = 0; i < (arvb->nchaves + 1); i++)
+        {
+            if(arvb->filho[i]->nchaves == (2*t - 1)){
+                arvb = Divide(arvb, arvb->filho[i], t);
+            }
+        }}
+    
+   
+    //Caso Trivial: Nó é folha e tem menos que 2t-1 chaves
+    if(arvb->folha && arvb->nchaves < (2*t-1)){
         int i = arvb->nchaves - 1;
 
         while (i>=0 && arvb->chave[i] > num)
@@ -70,8 +163,16 @@ TARVB* TARVB_insere(TARVB* arvb, int num, int t){
         arvb->chave[i+1] = num;
         arvb->nchaves++;
 
-        
+        return arvb;
     }
+
+    //Caso 3: Nó é interno e nada precisa ser dividido aqui
+
+    int i = 0;
+    while (i < (arvb->nchaves) && arvb->chave[i] < num ) i++;
+
+    arvb->filho[i] = TARVB_insere(arvb->filho[i], num, t);
+    
 
     return arvb;
 }
--- libs/TARVB/teste_TARVB.c
+++ libs/TARVB/teste_TARVB.c
@@ -5,9 +5,18 @@ int main(){
 
     TARVB* arvb = inicializa();
 
-    arvb = TARVB_insere(arvb, 25, 2);
+    arvb = TARVB_insere(arvb, 2, 2);
+    arvb = TARVB_insere(arvb, 4, 2);
+    arvb = TARVB_insere(arvb, 6, 2);
+    arvb = TARVB_insere(arvb, 3, 2);
+    arvb = TARVB_insere(arvb, 9, 2);
+    arvb = TARVB_insere(arvb, 7, 2);
+    arvb = TARVB_insere(arvb, 8, 2);
+    arvb = TARVB_insere(arvb, 1, 2);
+    arvb = TARVB_insere(arvb, 0, 2);
     arvb = TARVB_insere(arvb, 10, 2);
-    arvb = TARVB_insere(arvb, 12, 2);
+
+    printf("\n\n\n%d", arvb->nchaves);
 
     // printf("\n%p\n", arvb->filho[0]);
 
